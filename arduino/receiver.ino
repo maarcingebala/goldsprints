bf48db1a7690357b474eba34327775ffb6a9bc48
Marcin Gębala <maarcin.gebala@gmail.com>
2016-07-29 19:33:25 +0200

    Update arduino code to handle to sensors

--- arduino/receiver.ino
+++ arduino/receiver.ino
@@ -1,29 +1,54 @@
-volatile int revolutions = 0;
-float bikespeed;
+volatile int revolutionsFirstPlayer = 0;
+volatile int revolutionsSecondPlayer= 0;
 double wheelcirc = 2.13;
-double rpm;
-double rpmB;
-double vbi;
+double rollerToBicycleWheel = 6.196364;
+double circuitWheel = 2.13;
+double meterPerSecondToKmPerHour = 3.6;
+double rpmRollerFirstPlayer;
+double rpmBicycleFirstPlayer;
+double rpmRollerSecondPlayer;
+double rpmBicycleSecondPlayer;
+double bicycleSpeedFirstPlayer;
+double bicycleSpeedSecondPlayer;
 double lastmillis;
+
 
 void setup() {
   pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
   Serial.begin(9600);
   digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
   attachInterrupt(0, rpm_bicycle, RISING);
+  attachInterrupt(1, rpm_bicycle2, RISING);
 }
 
 void loop() {
- if (revolutions >= 1 ) {
-   rpm = (revolutions / ((millis() - lastmillis) * 0.001));
-   rpmB = rpm / 6.196364;
-   vbi = rpmB * 2.13 * 3.6;
+ if (revolutionsFirstPlayer >= 1 ) {
+   rpmRollerFirstPlayer = (revolutionsFirstPlayer / ((millis() - lastmillis) * 0.001));
+   rpmBicycleFirstPlayer = rpmRollerFirstPlayer / rollerToBicycleWheel;
+   bicycleSpeedFirstPlayer = rpmBicycleFirstPlayer * circuitWheel * meterPerSecondToKmPerHour ;
    lastmillis = millis();
-   Serial.println(vbi);
-   revolutions = 0;
+   Serial.print("a|");
+   Serial.println(bicycleSpeedFirstPlayer);
+   revolutionsFirstPlayer = 0;
+ }
+  if (revolutionsSecondPlayer >= 1 ) {
+   rpmRollerSecondPlayer = (revolutionsSecondPlayer / ((millis() - lastmillis) * 0.001));
+   rpmBicycleFirstPlayer = rpmRollerFirstPlayer / rollerToBicycleWheel;
+   bicycleSpeedSecondPlayer = rpmBicycleSecondPlayer * circuitWheel * meterPerSecondToKmPerHour;
+   lastmillis = millis();
+   Serial.print("b|");
+   Serial.println(bicycleSpeedSecondPlayer);
+   revolutionsSecondPlayer = 0;
  }
 }
 
+
 void rpm_bicycle() {
- revolutions++;
+ revolutionsFirstPlayer++;
+}
+
+void rpm_bicycle2() {
+ revolutionsSecondPlayer++;
 }
