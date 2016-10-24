3cbfc267f0e17dd894d2280bcbf6aa226fc87213
Marcin Gębala <maarcin.gebala@gmail.com>
2016-10-24 22:41:06 +0200

    Refactor names in arduino code

--- arduino/receiver.ino
+++ arduino/receiver.ino
@@ -1,55 +1,75 @@
-volatile int revolutionsFirstPlayer = 0;
-volatile int revolutionsSecondPlayer= 0;
-double wheelcirc = 2.13;
-double rollerToBicycleWheel = 6.196364;
+// constants
 double circuitWheel = 2.13;
-double meterPerSecondToKmPerHour = 3.6;
-double rpmRollerFirstPlayer;
-double rpmBicycleFirstPlayer;
-double rpmRollerSecondPlayer;
-double rpmBicycleSecondPlayer;
-double bicycleSpeedFirstPlayer;
-double bicycleSpeedSecondPlayer;
+double msToKmh = 3.6;
+double rollerToBicycleWheel = 6.196364;
+
+// variables
+double rpmRollerA;
+double rpmBicycleA;
+double rpmRollerB;
+double rpmBicycleB;
+double speedA;
+double speedB;
 double lastmillisFirst;
 double lastmillisSecond;
 
+volatile int counterA = 0;  // revolutions counter for bike A
+volatile int counterB = 0;  // revolutions counter for bike B
+
+
+// pins
+int vccA = 8;
+int vccB = 9;
+
+// https://www.arduino.cc/en/Reference/attachInterrupt
+int sensorA = 0;  // interrupt 0 -> pin 2 (Arduino Uno)
+int sensorB = 1;  // interrupt 1 -> pin 3 (Arduino Uno)
+
 
 void setup() {
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
+  // setup bike A
+  pinMode(vccA, OUTPUT);  
+  digitalWrite(vccA, HIGH);
+  attachInterrupt(sensorA, onSignalA, RISING);
+
+  // setup bike B
+  pinMode(vccB, OUTPUT);
+  digitalWrite(vccB, HIGH);
+  attachInterrupt(sensorB, onSignalB, RISING);
+
+  // setup serial
   Serial.begin(9600);
-  digitalWrite(8, HIGH);
-  digitalWrite(9, HIGH);
-  attachInterrupt(0, rpm_bicycle, RISING);
-  attachInterrupt(1, rpm_bicycle2, RISING);
 }
 
 void loop() {
-  if (revolutionsFirstPlayer >= 1 ) {
-    rpmRollerFirstPlayer = (revolutionsFirstPlayer / ((millis() - lastmillisFirst) * 0.001));
-    rpmBicycleFirstPlayer = rpmRollerFirstPlayer / rollerToBicycleWheel;
-    bicycleSpeedFirstPlayer = rpmBicycleFirstPlayer * circuitWheel * meterPerSecondToKmPerHour ;
+  if (counterA >= 1 ) {
+    rpmRollerA = (counterA / ((millis() - lastmillisFirst) * 0.001));
+    rpmBicycleA = rpmRollerA / rollerToBicycleWheel;
+    speedA = rpmBicycleA * circuitWheel * msToKmh ;
+
     lastmillisFirst = millis();
     Serial.print("a|");
-    Serial.println(bicycleSpeedFirstPlayer);
-    revolutionsFirstPlayer = 0;
+    Serial.println(speedA);
+    counterA = 0;
   }
-  if (revolutionsSecondPlayer >= 1 ) {
-    rpmRollerSecondPlayer = (revolutionsSecondPlayer / ((millis() - lastmillisSecond) * 0.001));
-    rpmBicycleSecondPlayer = rpmRollerSecondPlayer / rollerToBicycleWheel;
-    bicycleSpeedSecondPlayer = rpmBicycleSecondPlayer * circuitWheel * meterPerSecondToKmPerHour;
+
+  if (counterB >= 1 ) {
+    rpmRollerB = (counterB / ((millis() - lastmillisSecond) * 0.001));
+    rpmBicycleB = rpmRollerB / rollerToBicycleWheel;
+    speedB = rpmBicycleB * circuitWheel * msToKmh;
+
     lastmillisSecond = millis();
     Serial.print("b|");
-    Serial.println(bicycleSpeedSecondPlayer);
-    revolutionsSecondPlayer = 0;
+    Serial.println(speedB);
+    counterB = 0;
   }
 }
 
 
-void rpm_bicycle() {
-  revolutionsFirstPlayer++;
+void onSignalA() {
+  counterA++;
 }
 
-void rpm_bicycle2() {
-  revolutionsSecondPlayer++;
+void onSignalB() {
+  counterB++;
 }
