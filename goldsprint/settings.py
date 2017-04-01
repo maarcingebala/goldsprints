0b3d1e86ddf99dec0c37ae28e8486704ab98bc85
Marcin Gębala <maarcin.gebala@gmail.com>
2017-04-01 16:13:01 +0200

    Change DB name

--- goldsprint/settings.py
+++ goldsprint/settings.py
@@ -62,7 +62,7 @@ WSGI_APPLICATION = 'goldsprint.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'goldsprint-db',
+        'NAME': 'db.sqlite',
     }
 }
 
